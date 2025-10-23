94b87cd396a444ef126f86d50b347d24236edc29
cjrodenb-boop <cjrodenb@iu.edu>
2025-10-22 23:15:17 -0400

    starting added method headers

---
+++ C292 Main Project/Assets/Scripts/screenSwitchScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class screenSwitchScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject questionScreen;
+    [SerializeField] private GameObject answerScreen;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    public void Question()
+    {
+
+    }
+
+    public void Answer()
+    {
+
+    }
+
+
+}
