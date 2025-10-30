cd6d46703c80415a324ff408529f34f37d6015b4
cjrodenb-boop <cjrodenb@iu.edu>
2025-10-30 12:40:26 -0400

    wrote method but not working

--- C292 Main Project/Assets/Scripts/answersManager.cs
+++ C292 Main Project/Assets/Scripts/answersManager.cs
@@ -2,25 +2,46 @@ using UnityEngine;
 
 public class answersManager : MonoBehaviour
 {
+    [SerializeField] private Button[] buttons;
 
-   
-
-    private string[] questions = new string[]
+    private string[] answers = new string[]
     {
-        "Nigeria", "Kenya", "Ethiopia", "Ghana"
+        "Nigeria", "Kenya", "Ethiopia", "Ghana", "China", "India", "Japan", "Thailand",
+        "Philippines", "Mongolia", "Bhutan", "United States", "Cuba", "Brazil", "Chile",
+        "Peru", "France", "Germany", "Italy", "Spain", "Poland", "Norway", "Albania",
+        "Austrailia", "Kiribati"
     };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         AnswerPicker();
-
     }
     
     public void AnswerPicker()
     {
-        
-    }
+        List<string> copy = new List<string>(answers);
 
-    
+        List<string> selected = new List<string>();
+        for (int i = 0; i < 4; i++)
+        {
+            int idx = Random.Range(0, copy.Count);
+            selected.Add(answersCopy[idx]);
+            copy.RemoveAt(idx);
+        }
+
+        for (int i = 0; i < selected.Count; i++)
+        {
+            string temp = selected[i];
+            int idx = Random.Range(i, selected.Count);
+            selected[i] = selected[idx];
+            selected[idx] = temp;
+        }
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            Text buttonText = buttons[i].GetComponentInChildren<Text>(); // im not sure if this is correct
+            buttonText.text = selected[i];
+        }  
+    } 
 }
