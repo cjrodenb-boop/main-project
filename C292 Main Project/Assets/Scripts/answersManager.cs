f10a0bf1f551b74158b267b504925b9bd9b03fef
cjrodenb-boop <cjrodenb@iu.edu>
2025-10-30 11:04:56 -0400

    scene fix

---
+++ C292 Main Project/Assets/Scripts/answersManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class answersManager : MonoBehaviour
+{
+
+   
+
+    private string[] questions = new string[]
+    {
+        "Nigeria", "Kenya", "Ethiopia", "Ghana"
+    };
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AnswerPicker();
+
+    }
+    
+    public void AnswerPicker()
+    {
+        
+    }
+
+    
+}
