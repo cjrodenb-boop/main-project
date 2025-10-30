638355b8509f9d0552b5141b0f049aa1e06f0012
cjrodenb-boop <cjrodenb@iu.edu>
2025-10-30 16:59:56 -0400

    commit

--- C292 Main Project/Assets/Scripts/screenSwitchScript.cs
+++ C292 Main Project/Assets/Scripts/screenSwitchScript.cs
@@ -6,6 +6,7 @@ public class screenSwitchScript : MonoBehaviour
     [SerializeField] private GameObject questionScreen;
     [SerializeField] private GameObject answerScreen;
     [SerializeField] private questionManager qm;
+    [SerializeField] private answersManager am;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,6 +15,7 @@ public class screenSwitchScript : MonoBehaviour
         questionScreen.SetActive(true);
         answerScreen.SetActive(false);
         qm.QuestionPicker();
+        am.AnswerPicker();
     }
 
     public void Question()
@@ -21,6 +23,7 @@ public class screenSwitchScript : MonoBehaviour
         questionScreen.SetActive(true);
         answerScreen.SetActive(false);
         qm.QuestionPicker();
+        am.AnswerPicker();
     }
 
     public void Answer()
