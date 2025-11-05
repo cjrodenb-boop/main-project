bbedea758a132d465fa03090fecd58b9bee6a4e1
cjrodenb-boop <cjrodenb@iu.edu>
2025-11-05 17:19:07 -0600

    added script to change the color of the buttons when clicked. added method header

---
+++ C292 Main Project/Assets/Scripts/answerButtonScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class answerButtonScript : MonoBehaviour
+{
+    public TMP_Text buttonText;
+    public questionManager qm; 
+    
+    public void OnClick()
+    {
+        
+    }
+    
+}
