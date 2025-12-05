c9434f454240a20b70331af07db0938ee18185d1
cjrodenb-boop <cjrodenb@iu.edu>
2025-12-04 22:50:15 -0500

    removed this during the submission video

--- C292 Main Project/Assets/Scripts/questionManager.cs
+++ C292 Main Project/Assets/Scripts/questionManager.cs
@@ -98,7 +98,7 @@ public class questionManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //QuestionPicker();
+     
     }
 
 
